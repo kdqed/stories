f85e8f12ef42444aa4a7983470e221d7a60346b9
Karthik D <krtdvn@gmail.com>
2020-10-20 19:48:24 +0530

    f

--- generate.py
+++ generate.py
@@ -12,9 +12,11 @@ with open('template/story.html') as f:
 for mdfilename in os.listdir('md'):
   i = int(mdfilename.split(".")[0])
   mdcontent = open('md/'+mdfilename).read()
+  title = mdcontent.split("\n",2)[0].split("#")[1].strip()
+  print(i,title)
   main_html = mdparser(mdcontent)
   with open(str(i)+".html",'w') as f:
-    content = template.render(i=i,main_html=main_html)
+    content = template.render(i=i,main_html=main_html,title=title)
     f.write(content)
     f.close()
   
---
+++ push.sh
@@ -0,0 +1,4 @@
+git add *
+git stage *
+git commit -m "f"
+git push
